7316595375237d56b6409670133f1e3b5250b6e8
chankyu <bck@wishket.com>
2020-10-20 21:39:09 +0900

    indexview url

--- ebookFinder/urls.py
+++ ebookFinder/urls.py
@@ -2,8 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from ebookFinder.apps.book.views import IndexView
 
 urlpatterns = [
+    path('', IndexView.as_view()),
     path('admin/', admin.site.urls),
     path('book/', include('ebookFinder.apps.book.urls')),
 ]
