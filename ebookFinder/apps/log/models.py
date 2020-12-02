2d64a6b9653b8d827ed30877c11e07b63f9a9a0d
chankyu <bck@wishket.com>
2020-12-03 00:32:59 +0900

    modify SearchHistory

--- ebookFinder/apps/log/models.py
+++ ebookFinder/apps/log/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class SearchHistory(models.Model):
-    q = models.CharField(max_length=200)
+    q = models.CharField(max_length=200, default='')
     user_identifier = models.CharField(max_length=50, default='')
 
     dt_created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-date_created']
+        ordering = ['-dt_created']
