ddcf4c8871776abcf083e4a2521e44f3ec7405d5
chankoo <ckbaek1125@gmail.com>
2024-01-26 16:24:12 +0900

    deploy: set concurrency limits

--- ebookFinder/docker-entrypoint.sh
+++ ebookFinder/docker-entrypoint.sh
@@ -5,5 +5,6 @@ find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 python manage.py makemigrations && python manage.py migrate
 
 mkdir -p ./logs/django && touch ./logs/django/error.log
+mkdir -p ./logs/uvicorn && touch ./logs/uvicorn/error.log
 
-uvicorn asgi:application --workers 1 --host 0.0.0.0 --port 8000 --reload
+uvicorn asgi:application --workers 1 --host 0.0.0.0 --port 8000 --reload --limit-concurrency 128 --log-level error --log-config ./logs/uvicorn/log.ini
