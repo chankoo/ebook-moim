30ade1fa572ee6775c188b42b8fe1ebdd770ee4e
chankoo <ckbaek1125@gmail.com>
2024-02-23 20:09:38 +0900

    fix: valid ISBN-10

--- ebookFinder/apps/book/utils.py
+++ ebookFinder/apps/book/utils.py
@@ -5,8 +5,10 @@ def get_valid_isbn(isbn_slug: str) -> str:
 
     if '-' in isbn_slug:
         isbn10, isbn13 = isbn_slug.split('-')
-        if is_valid_isbn10(isbn10) and is_valid_isbn13(isbn13):
+        if is_valid_isbn10(isbn10):
             return isbn10
+        if is_valid_isbn13(isbn13):
+            return isbn13
     
     if len(isbn_slug) == 10 and is_valid_isbn10(isbn_slug):
         return isbn_slug
@@ -19,8 +21,8 @@ def is_valid_isbn10(isbn):
     if not isinstance(isbn, str):
         return False
 
-    # ISBN-10은 숫자와 마지막에 'X'로 이루어짐
-    if not isbn[:-1].isdigit() or (isbn[-1] != 'X' and not isbn[-1].isdigit()):
+    # ISBN-10은 숫자와 마지막에 'x'로 이루어짐
+    if not isbn[:-1].isdigit() or (isbn[-1] != 'x' and not isbn[-1].isdigit()):
         return False
 
     # ISBN-10의 길이는 10
@@ -32,7 +34,7 @@ def is_valid_isbn10(isbn):
     check_digit = total % 11
 
     # 마지막 자리 검사
-    if isbn[-1] == 'X':
+    if isbn[-1] == 'x':
         return check_digit == 10
     else:
         return check_digit == int(isbn[-1])
